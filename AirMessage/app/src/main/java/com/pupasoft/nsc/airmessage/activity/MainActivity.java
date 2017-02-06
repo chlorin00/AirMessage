93e365b35c1f01913720c78884e82ca0c60c80bf
chlorin00 <wong_839@hotmail.com>
2017-02-06 16:57:25 +0700

    update navigationview drawer & ui

--- AirMessage/app/src/main/java/com/pupasoft/nsc/airmessage/activity/MainActivity.java
+++ AirMessage/app/src/main/java/com/pupasoft/nsc/airmessage/activity/MainActivity.java
@@ -1,17 +1,15 @@
 package com.pupasoft.nsc.airmessage.activity;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.PersistableBundle;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.pupasoft.nsc.airmessage.R;
 import com.pupasoft.nsc.airmessage.fragment.MessageFragment;
@@ -19,8 +17,12 @@ import com.pupasoft.nsc.airmessage.fragment.MessageFragment;
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    View navHeader;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
+
+    private String[] menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +42,38 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.drawerMenu);
+
+        menuList = getResources().getStringArray(R.array.drawer_menu);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {     //TODO: Handle drawer menu button.
+                    case R.id.drawerHome:
+                        Log.d("I'm here", "Home Activity");
+//                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.drawerLocation:
+                        Log.d("I'm here", "Location Activity");
+//                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.drawerLogout:
+                        Log.d("I'm here", "Log out");
+//                        drawerLayout.closeDrawers();
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(
                 MainActivity.this,
                 drawerLayout,
                 R.string.open_drawer,
                 R.string.close_drawer
-                );
+        );
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
